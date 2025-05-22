114389285e52e71a9db1cb98f00d8a7e3b6ffed1
deepakc <deepak.c@capgemini.com>
2025-05-22 13:30:18 +0530

    Add New inventory button inclusion

--- src/main/java/com/inventory/controller/InventoryController.java
+++ src/main/java/com/inventory/controller/InventoryController.java
@@ -1,7 +1,11 @@
 package com.inventory.controller;
 
 import com.inventory.model.Inventory;
+import com.inventory.model.MasterProduct;
 import com.inventory.service.InventoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,15 +14,22 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/inventory")
 public class InventoryController {
+    private static final Logger log = LoggerFactory.getLogger(InventoryController.class);
     @Autowired
     private InventoryService service;
 
     @GetMapping
     public List<Inventory> getAllItems() {
         return service.getAllItems();
+    }
+
+    @GetMapping ("master-products")
+    public List<MasterProduct> getMasterProducts() {
+        return service.getAllProducts();
     }
 
     @GetMapping("/{id}")
@@ -38,6 +49,14 @@ public class InventoryController {
         return service.addItem(productName, item);
     }
 
+    @PostMapping("/addProduct")
+    public MasterProduct addProduct(@RequestBody Map<String, Object> request) {
+        MasterProduct item = new MasterProduct();
+        item.setProductName((String) request.get("addProductName"));
+        return service.addProduct(item);
+    }
+
+
     @PutMapping("/{id}")
     public Inventory updateItem(@PathVariable Long id, @RequestBody Map<String, Object> request) {
         String productName = (String) request.get("productName");
@@ -56,10 +75,13 @@ public class InventoryController {
         String productName = (String) request.get("productName");
 
         Inventory itemDetails = new Inventory();
+        itemDetails.setId(id);
         itemDetails.setColor((String) request.get("color"));
         itemDetails.setCategory((String) request.get("category"));
         itemDetails.setYardAvailable(Double.parseDouble(request.get("yardAvailable").toString()));
         itemDetails.setPieceAvailable(Double.parseDouble(request.get("pieceAvailable").toString()));
+
+        log.info("Item Details: {}", itemDetails);
 
         return service.updateItemCond(id, productName, itemDetails,cond);
     }
--- src/main/java/com/inventory/service/InventoryService.java
+++ src/main/java/com/inventory/service/InventoryService.java
@@ -1,15 +1,19 @@
 package com.inventory.service;
 
+import com.inventory.controller.InventoryController;
 import com.inventory.model.Inventory;
 import com.inventory.model.MasterProduct;
 import com.inventory.repository.InventoryRepository;
 import com.inventory.repository.MasterProductRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
-
+@Slf4j
 @Service
 public class InventoryService {
     @Autowired
@@ -17,9 +21,14 @@ public class InventoryService {
 
     @Autowired
     private MasterProductRepository masterProductRepository;
+    private static final Logger log = LoggerFactory.getLogger(InventoryService.class);
 
     public List<Inventory> getAllItems() {
         return inventoryRepository.findAll();
+    }
+
+    public List<MasterProduct> getAllProducts() {
+        return masterProductRepository.findAll();
     }
 
     public Optional<Inventory> getItemById(Long id) {
@@ -39,8 +48,13 @@ public class InventoryService {
         return inventoryRepository.save(item);
     }
 
+    public MasterProduct addProduct(MasterProduct product) {
+        return masterProductRepository.save(product);
+    }
+
     public Inventory updateItem(Long id, String productName, Inventory itemDetails) {
         Optional<Inventory> optionalItem = inventoryRepository.findById(id);
+        log.info("Inside updateItem ");
         if (optionalItem.isPresent()) {
             Inventory item = optionalItem.get();
 
@@ -64,8 +78,11 @@ public class InventoryService {
 
     public Inventory updateItemCond(Long id, String productName, Inventory itemDetails, String cond) {
         Optional<Inventory> optionalItem = inventoryRepository.findById(id);
+        log.info("Updating item with optionalItem : {} {} {}", optionalItem.isPresent(), id, cond);
         if (optionalItem.isPresent()) {
             Inventory item = optionalItem.get();
+
+            log.info("Updating item with ID: {} and item: {}", id,item);
 
             // Find or create the product in MasterProduct table
             MasterProduct product = masterProductRepository.findByProductName(productName)
@@ -81,6 +98,8 @@ public class InventoryService {
             //http://localhost:8080/api/inventory/updatecond/5/Procure
             if(cond.equals("Procure"))
             {
+
+                log.info("inside Procure");
                 double yard = item.getYardAvailable()+itemDetails.getYardAvailable();
                 double piece = item.getPieceAvailable() + itemDetails.getPieceAvailable();
                 item.setYardAvailable(yard);
@@ -89,9 +108,12 @@ public class InventoryService {
                 double procurePieces = item.getProcurementPieces()+itemDetails.getPieceAvailable();
                 item.setProcurementYards(procureYards);
                 item.setProcurementPieces(procurePieces);
+                log.info("inside Procure - {}",item);
             }
             //http://localhost:8080/api/inventory/updatecond/5/Sales
             else if (cond.equals("Sales")) {
+
+                log.info("inside Sales");
                 double yard = item.getYardAvailable()-itemDetails.getYardAvailable();
                 double piece = item.getPieceAvailable() - itemDetails.getPieceAvailable();
                 item.setYardAvailable(yard);
@@ -100,15 +122,18 @@ public class InventoryService {
                 double salePieces = item.getSalePieces()+itemDetails.getPieceAvailable();
                 item.setSaleYards(saleYards);
                 item.setSalePieces(salePieces);
+                log.info("inside Sales - {}",item);
             }
             //http://localhost:8080/api/inventory/updatecond/5/Hold
             else if (cond.equals("Hold")) {
+                log.info("inside Hold");
                 double yard = item.getYardAvailable()-itemDetails.getYardAvailable();
                 double piece = item.getPieceAvailable() - itemDetails.getPieceAvailable();
                 item.setYardAvailable(yard);
                 item.setPieceAvailable(piece);
                 item.setPiecesOnHold(itemDetails.getPieceAvailable());
                 item.setYardsOnHold(itemDetails.getYardAvailable());
+                log.info("inside Hold - {}",item);
             } else {
                 item.setYardAvailable(itemDetails.getYardAvailable());
                 item.setPieceAvailable(itemDetails.getPieceAvailable());
